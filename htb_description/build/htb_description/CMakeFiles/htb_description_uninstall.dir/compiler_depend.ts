6cc655cb8b0dab079c9ef5418a056ccaf3fb934d
Martin Nievas <martin.nievas.ar@gmail.com>
2023-01-23 18:23:57 -0300

    gitignore update

--- htb_description/build/htb_description/CMakeFiles/htb_description_uninstall.dir/compiler_depend.ts
+++
@@ -1,2 +0,0 @@
-# CMAKE generated file: DO NOT EDIT!
-# Timestamp file for custom commands dependencies management for htb_description_uninstall.
--- htb_description/build/htb_description/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /mnt/DatosLinux/repositorios/hover-table-bot-HTB/htb_description
-# Build directory: /mnt/DatosLinux/repositorios/hover-table-bot-HTB/htb_description/build/htb_description
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
